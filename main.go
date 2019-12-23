b86ad68b04fc34ae0c4fcdc735afb314265bf9ca
F9Uf <atoseka@gmail.com>
2019-12-23 12:17:44 +0700

    success

--- main.go
+++ main.go
@@ -1,64 +1,38 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	e := echo.New()
-	e.POST("/add", addHandler)
-	e.POST("/mul", mulHandler)
-	e.Start(":8080")
-}
+	ch := make(chan string, 10)
+	go findImages(ch)
 
-type addRequest struct {
-	A int
-	B int
-}
-
-type addResponse struct {
-	Result int `json:"res"`
-}
-
-func addHandler(c echo.Context) error {
-	// parse request
-	var reqBody addRequest
-	err := c.Bind(&reqBody)
-	if err != nil {
-		return err
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			for filename := range ch {
+				resizeImage(filename)
+			}
+			wg.Done()
+		}()
 	}
-
-	// calculate
-	result := reqBody.A + reqBody.B
-
-	// send response
-	return c.JSON(200, addResponse{
-		Result: result,
-	})
+	wg.Wait()
 }
 
-type mulRequest struct {
-	A int
-	B int
-}
-
-type mulResponse struct {
-	Result int `json:"res"`
-}
-
-func mulHandler(c echo.Context) error {
-	// parse request
-	var reqBody mulRequest
-	err := c.Bind(&reqBody)
-	if err != nil {
-		return err
+func findImages(ch chan string) {
+	for i := 1; i <= 20; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("Found file-%d\n", i)
+		ch <- fmt.Sprintf("file-%d", i)
 	}
+	close(ch)
+}
 
-	// calculate
-	result := reqBody.A * reqBody.B
-
-	// send response
-	return c.JSON(200, mulResponse{
-		Result: result,
-	})
+func resizeImage(filename string) {
+	fmt.Printf("resize image: %s\n", filename)
+	time.Sleep(2 * time.Second)
 }
